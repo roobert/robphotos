5f15e8d1bbecc4275d9b78b882f35ba046dec187
Rob Wilson <roobert@gmail.com>
2014-09-21 20:37:08 +0100

    initial commit

---
+++ app.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+
+require 'sinatra'
+require 'nokogiri'
+require 'redcarpet'
+require 'ostruct'
+
+require 'haml'
+require 'sass'
+
+require 'ap'
+require 'pp'
+require 'yaml'
+
+# this auto-reloads files with changed mtime
+Sinatra::Application.reset!
+
+set :haml, { :ugly=>true }
+
+get '/css/:style.css' do
+  scss :"#{params[:style]}"
+end
+
+get '/' do
+  @images = YAML.load_file File.join(File.dirname(__FILE__), "public/images.yaml")
+  haml :index
+end
